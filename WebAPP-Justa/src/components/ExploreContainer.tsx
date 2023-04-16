865f78b3003ea860dc33e1e798f3bb1fc65ad3da
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-04-15 22:04:49 -0300

    init web app

---
+++ WebAPP-Justa/src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Login</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
