c32155734bbfb20310294d4069266d19d0870be0
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-04-16 23:39:57 -0300

    add novas páginas e icons

--- WebAPP-Justa/src/pages/Home.tsx
+++ WebAPP-Justa/src/pages/Home.tsx
@@ -1,17 +1,16 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonLabel, IonItem, IonInput, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonItem, IonInput, IonButton, IonIcon, IonMenu, IonFab, IonImg} from '@ionic/react';
 import './Home.css';
-import {} from 'ionicons/icons';
+import { lockClosedOutline, mailOutline } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-
-      <IonHeader collapse="fade">
+      <IonHeader>
         <IonToolbar>
           <IonTitle>Justa</IonTitle>
+          <IonMenu></IonMenu>
         </IonToolbar>
       </IonHeader>
-
 
       <IonContent>
         <div className="login-section ion-padding">
@@ -22,22 +21,23 @@ const Home: React.FC = () => {
           <div className="login-form ion-padding">
             <div className="form-input">
               <IonItem>
-                <IonLabel position="floating">E-mail</IonLabel>
-                <IonInput type="email"></IonInput>
+                <IonIcon className="iconsInput" icon={mailOutline}></IonIcon>
+                <IonLabel className = "labelInput" position="floating">E-mail</IonLabel>
+                <IonInput className = "Input" type="email"></IonInput>
               </IonItem>
             </div>
             <div className="form-input">
-              <IonIcon name = "lock-closed-outline"></IonIcon>
               <IonItem>
-                <IonLabel position="floating">Senha</IonLabel>
-                <IonInput type="password"></IonInput>
+                <IonIcon className="iconsInput" icon={lockClosedOutline}></IonIcon>
+                <IonLabel className = "labelInput" position="floating">Senha</IonLabel>
+                <IonInput className = "Input" type="password"></IonInput>
               </IonItem>
             </div>
           </div>
           <div className="action-button ion-padding">
-            <IonButton size="default" class="login-button">Login</IonButton>
+            <IonButton size="default" class="login-button" routerLink="/signin">Entrar</IonButton>
             <p>Esqueceu a sua senha?</p>
-            <IonButton class="signup-button" size="default" fill="outline" routerLink="\signup">Sign up</IonButton>
+            <IonButton class="signup-button" size="default" fill="outline" routerLink="/signup">Sign up</IonButton>
           </div>
         </div>
       </IonContent>
@@ -47,3 +47,4 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
---
+++ WebAPP-Justa/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { IonHeader, IonMenu, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
+const Signin = () =>{
+    return(
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Justa</IonTitle>
+                    <IonMenu></IonMenu>
+                </IonToolbar>
+            </IonHeader>
+            </IonPage>
+    );
+}
+
+export default Signin
