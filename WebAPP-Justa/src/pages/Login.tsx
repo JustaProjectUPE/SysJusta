2748b565783ed3d1446e3b56deb3c19cc17bcdef
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-04-19 01:37:57 -0300

    inicio do menu

--- WebAPP-Justa/src/pages/Login.tsx
+++ WebAPP-Justa/src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import { IonButtons, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonIcon } from "@ionic/react";
-import { arrowForwardOutline } from "ionicons/icons";
+import { IonButtons,IonItem, IonList, IonLabel, IonAvatar, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonIcon } from "@ionic/react";
+import { arrowForwardOutline, notificationsOutline, settingsOutline, logOutOutline} from "ionicons/icons";
 import './Login.css'
 import { Balance } from "../components/balance";
 
@@ -7,22 +7,59 @@ const Signin = () =>{
   let balance = 1;
 
     return(
-        <>
+      <>
         <IonMenu contentId="main-content">
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>Menu Content</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="background-menu ion-padding">This is the menu content.</IonContent>
-        </IonMenu>
+          <section className="header-section" >
+            <IonHeader>
+              <IonToolbar>
+                <div className="buttons-header">
+                  <IonButton id="settings" fill="clear">
+                    <IonIcon icon={settingsOutline} slot="end"></IonIcon>
+                  </IonButton>
+                  <IonButton id="notify"  fill="clear">
+                    <IonIcon icon={notificationsOutline} slot="end"></IonIcon>
+                  </IonButton>
+                  <IonButton id="logout" fill="clear" routerLink="/home">
+                    <IonIcon icon={logOutOutline} slot="start"></IonIcon> Sair
+                  </IonButton>
+                </div>
+              </IonToolbar>
+            </IonHeader>
+          </section>
+          <section className="avatar-header">
+            <IonItem lines="none">
+              <IonAvatar slot="start">
+                <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+              </IonAvatar>
+              <IonLabel>
+                Nome Sobrenome
+                <p>Nível 3</p>
+              </IonLabel>
+              <div className="icon-hero"><img src = "/superhero.png"></img></div>
+            </IonItem>
+          </section>
+          <IonContent className="options-menu">
+            <IonList>
+              <IonItem lines="none">
+                <IonLabel>Opção 1</IonLabel>
+              </IonItem>
+              <IonItem lines="none">
+                <IonLabel>Opção 2</IonLabel>
+              </IonItem>
+            </IonList>
+          </IonContent>
+          </IonMenu>
+
         <IonPage id="main-content">
           <IonHeader>
             <IonToolbar>
               <IonButtons slot="start">
-                <IonMenuButton></IonMenuButton>
+                <IonMenuButton>
+                </IonMenuButton>
               </IonButtons>
-              <IonTitle>Menu</IonTitle>
+              <div className="titleicon">
+                <img src = "/logo2.png"></img>
+              </div>
             </IonToolbar>
           </IonHeader>
           <IonContent>
