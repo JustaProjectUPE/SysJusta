8edf552003bc26c8575bd76ec428e49b80e791e3
Arthur Xavier <arthurcsxavier@gmail.com>
2023-04-18 21:04:06 -0300

    primeira implementacao de conteudo na pagina principal

--- WebAPP-Justa/src/pages/Login.tsx
+++ WebAPP-Justa/src/pages/Login.tsx
@@ -1,7 +1,11 @@
-import { IonButtons, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonContent } from "@ionic/react";
-import './Login.css';
+import { IonButtons, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonIcon } from "@ionic/react";
+import { arrowForwardOutline } from "ionicons/icons";
+import './Login.css'
+import { Balance } from "../components/balance";
 
 const Signin = () =>{
+  let balance = 1;
+
     return(
         <>
         <IonMenu contentId="main-content">
@@ -21,6 +25,44 @@ const Signin = () =>{
               <IonTitle>Menu</IonTitle>
             </IonToolbar>
           </IonHeader>
+          <IonContent>
+            <main>
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle>Nível de lealdade: 3</IonCardSubtitle>
+                  <IonCardTitle>SUPER-HERÓI</IonCardTitle>
+                  <IonButton fill="clear">
+                    Acesse os benefícios
+                    <IonIcon icon={arrowForwardOutline} slot="end"></IonIcon>
+                  </IonButton>
+                </IonCardHeader>
+              </IonCard>
+              <Balance balance={balance}/>
+              <section className="products-sec">
+                <IonTitle >Produtos</IonTitle>
+                <div className='products'>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only' ></IonIcon>
+                  </IonButton>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only' ></IonIcon>
+                  </IonButton>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only'></IonIcon>
+                  </IonButton>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only' ></IonIcon>
+                  </IonButton>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only'></IonIcon>
+                  </IonButton>
+                  <IonButton shape='round' size='large'>
+                    <IonIcon slot='icon-only'></IonIcon>
+                  </IonButton>
+                </div>
+              </section>
+            </main>
+          </IonContent>
         </IonPage>
       </>
     );
