148230c5ce0a55a3ff99594909732c50cca33021
Arthur Xavier <arthurcsxavier@gmail.com>
2023-04-20 10:06:58 -0300

    att rota de login - correcao de resposta

--- API/func/give.js
+++ API/func/give.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 async function giveData(client_tolken){
 
     const data = fs.readFileSync('db/db.json','utf-8');
-    const client = JSON.parse(data).extract
+    const client = JSON.parse(data).clients
 
     return client[client_tolken];
 
