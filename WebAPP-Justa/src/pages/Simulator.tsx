61417296cb688e94322dc0c6c5a4914cce10395b
Mozart Mendes Maia Filho <mozartmaiafilho@hotmail.com>
2023-04-21 21:37:15 -0300

    add ./simulator.css

--- WebAPP-Justa/src/pages/Simulator.tsx
+++ WebAPP-Justa/src/pages/Simulator.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption } from '@ionic/react';
+import './Simulator.css'
 
 const Simulator: React.FC = () => {
   const [valorTotal, setValorTotal] = useState(0);
