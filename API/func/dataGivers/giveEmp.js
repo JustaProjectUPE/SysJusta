5e91b505d7f527d5427f37ea1eddf366b670a783
Arthur Xavier <arthurcsxavier@gmail.com>
2023-04-24 09:25:11 -0300

    resultado por id do cliente

--- API/func/dataGivers/giveEmp.js
+++ API/func/dataGivers/giveEmp.js
@@ -5,7 +5,7 @@ function giveEmp(client_token){
     const data = fs.readFileSync('db/db.json','utf-8');
     const client_emp = JSON.parse(data).emp;
 
-    return [200, client_emp];
+    return [200, client_emp[client_token]];
 
 }
 
--- API/func/dataGivers/giveExtract.js
+++ API/func/dataGivers/giveExtract.js
@@ -5,7 +5,7 @@ function giveExtract(client_token){
     const data = fs.readFileSync('db/db.json','utf-8');
     const client_extract = JSON.parse(data).extract;
     
-    return [200, client_extract];
+    return [200, client_extract[client_token]];
 
 }
 
