9c23a2eaac40d6a9206e29ea6c8912aa01346b84
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-04-25 16:21:47 -0300

    add pag linkou + correção handleGoBack

--- WebAPP-Justa/src/pages/Linkou.tsx
+++ WebAPP-Justa/src/pages/Linkou.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { IonContent, IonPage, IonInput, IonButton } from '@ionic/react';
+import { IonContent, IonPage, IonInput, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonGrid, IonRow, IonCol, IonLabel, IonCardContent, IonCard, IonItem } from '@ionic/react';
 import './Linkou.css'
+import ReactInputMask from 'react-input-mask';
 
 const GeneratePaymentLinkPage = () => {
   const [amount, setAmount] = useState('');
@@ -15,20 +16,34 @@ const GeneratePaymentLinkPage = () => {
 
   return (
     <IonPage>
-      <IonContent className='form'>
-        <h1>Gerar link de pagamento</h1>
-        <IonInput
-          placeholder="Valor"
-          type="number"
-          value={amount}
-          onIonChange={(e) => setAmount(e.detail.value!)}
-        />
-        <IonInput
-          placeholder="Descrição"
-          type="text"
-          value={description}
-          onIonChange={(e) => setDescription(e.detail.value!)}
-        />
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton></IonBackButton>
+          </IonButtons>
+          <div className="titleicon">
+            <img src = "/logo2.png"></img>
+          </div>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className='linkou-form'>
+        <IonGrid className="linkou-section">
+          <IonRow>
+            <IonCol size="12">
+              <h1>Gerar link de pagamento</h1>
+              <IonCard>
+                <IonCardContent>
+                  <IonItem>
+                <IonLabel position="floating">Valor</IonLabel>
+                <IonInput color="medium" value={amount} min="0" clearInput={true} type="number" onIonChange={(e) => setAmount(e.detail.value!)}></IonInput>
+                </IonItem>
+                <IonItem>
+                <IonLabel position="floating">Descrição</IonLabel>
+                <IonInput color="medium" value={amount} min="0" clearInput={true} type="number" onIonChange={(e) => setDescription(e.detail.value!)}></IonInput>
+                </IonItem>
+                </IonCardContent>
+              </IonCard>
         <IonButton onClick={generatePaymentLink}>Gerar link de pagamento</IonButton>
         {paymentLink && (
           <div>
@@ -36,6 +51,9 @@ const GeneratePaymentLinkPage = () => {
             <a href={paymentLink}>{paymentLink}</a>
           </div>
         )}
+        </IonCol>
+        </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
