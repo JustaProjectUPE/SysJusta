74fa9913224679a561ec8c8670bfe938d48cf517
Mayanne Gomes <mayane.viviane0511@gmail.com>
2023-04-26 20:55:46 -0300

    css pag simulator

--- WebAPP-Justa/src/pages/empty.tsx
+++ WebAPP-Justa/src/pages/empty.tsx
@@ -2,8 +2,6 @@ import { IonContent, IonHeader, IonPage, IonButton, IonIcon, } from '@ionic/reac
 import { arrowBackOutline } from 'ionicons/icons';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './ForgotPassword.css';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { arrowBackOutline } from 'ionicons/icons';
 
 const Empty: React.FC = () => {
   const {state} = useLocation();
